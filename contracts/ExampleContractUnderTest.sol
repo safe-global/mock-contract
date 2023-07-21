8b696081fd766ac41db96955d22fd9447ffe4cdc
Mikhail <mveehkim@gmail.com>
2023-07-21 12:04:09 +0400

    Migrate to hardhat

--- contracts/ExampleContractUnderTest.sol
+++ contracts/ExampleContractUnderTest.sol
@@ -2,15 +2,15 @@
 
 pragma solidity >=0.8.0 <0.9.0;
 
-import './ComplexInterface.sol';
+import "./ComplexInterface.sol";
 
 contract ExampleContractUnderTest {
     ComplexInterface complexInterface;
-  
+
     constructor(address _complexInterface) {
         complexInterface = ComplexInterface(_complexInterface);
     }
-   
+
     function callMockedFunction3Times() public view returns (bool) {
         complexInterface.acceptUintReturnUintView(1);
         complexInterface.acceptUintReturnUintView(1);
@@ -18,12 +18,12 @@ contract ExampleContractUnderTest {
         return true;
     }
 
-    function callMethodThatReturnsAddress() public returns (address) {
-        address foo = complexInterface.acceptUintReturnAddress(1);
+    function callMethodThatReturnsAddress() public view returns (address) {
+        address foo = complexInterface.acceptUintReturnAddressView(1);
         return foo;
     }
 
-    function callMethodThatReturnsBool() public returns (bool) {
-        return complexInterface.acceptUintReturnBool(1);
+    function callMethodThatReturnsBool() public view returns (bool) {
+        return complexInterface.acceptUintReturnBoolView(1);
     }
 }
